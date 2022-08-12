6bf6e9d372a92b0963c515fa0ac375b0d82e0a21
mldamico <matias.lioneldamico@gmail.com>
2022-08-12 09:45:16 -0300

    swap service added swap

--- src/swap/swap.service.ts
+++ src/swap/swap.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateSwapDto } from './dto';
 
 @Injectable()
 export class SwapService {
   constructor(private prismaService: PrismaService) {}
+
+  swap(dto: CreateSwapDto) {
+    return this.prismaService.swap.create({
+      data: {
+        position: dto.position,
+        quarter: dto.quarter,
+        time: dto.time,
+        timePlaying: dto.timePlaying,
+        enteringPlayerId: dto.enteringPlayerId,
+        leavingPlayerId: dto.enteringPlayerId,
+        matchId: dto.matchId,
+      },
+    });
+  }
 }
